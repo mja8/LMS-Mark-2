78542912e8f06d999204ca12716332eed33a67eb
mja8 <jaykr8107@gmail.com>
2025-09-03 01:04:08 +0530

    error in user.controller.js for uploading video

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -166,7 +166,7 @@ export const updateProfile = async (req,res) => {
         // extract public id of the old image from the url if it exists;
         if(user.photoUrl){
             const publicId = user.photoUrl.split("/").pop().split(".")[0]; // extract public id
-            deleteMediaFrom(publicId); // delete old photo (nya photo add krne se pehle)
+            deleteMediaFromCloudinary(publicId); // delete old photo (nya photo add krne se pehle)
         }
 
         // upload new photo (cloud pe upload kro photo ko)
